c58a0207490946b7bdd182565349e7ca3ed02fe0
whopper-cemnl <wjhopper510@gmail.com>
2016-01-19 09:16:58 -0500

    added new function drawCueTarget to handle all the calls to DrawFormattedText for displaying the word stimuli, this will be useful since the same spacign and formatting should be used at test time. Also changed study function to return a vector, not a whole table.

--- study.m
+++ study.m
@@ -1,13 +1,10 @@
-function data = study(data, window, constants)
+function onsets = study(data, window, constants)
 
-onsets = data.onset;
+onsets = nan(size(data,1),1);
 Screen('TextSize', window, 40);
 wakeup = Screen('Flip', window);
 for j = 1:size(data,1);
-    DrawFormattedText(window,data.cue{j},'right', 'center',[],[],[],[],[],[],constants.left_half-[0 0 constants.spacing 0]);
-    DrawFormattedText(window,' - ', 'center','center');
-    DrawFormattedText(window,data.target{j},constants.right_half(1)+constants.spacing, 'center');
-    Screen('DrawingFinished', window);
+    drawCueTarget(data.cue{j}, data.target{j}, window, constants)    
     vbl = Screen('Flip', window, wakeup + (constants.ifi/2));
     onsets(j) = vbl;
 	wakeup = WaitSecs('UntilTime', vbl + constants.cueDur - constants.ifi);
