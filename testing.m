8479576c40af3263f8d73c9538862f35d2dff258
whopper-cemnl <wjhopper510@gmail.com>
2016-01-21 12:48:57 -0500

    changed while loop where screen is updated with input to draw the question mark prompt as long as no input has been given by the user yet. Old version only drew the prompt for one flip and then drew an empty string in its place

--- testing.m
+++ testing.m
@@ -5,28 +5,32 @@ onset = nan(size(data,1),1);
 response = cell(size(data,1),1);
 firstPress = nan(size(data,1),1);
 lastPress = nan(size(data,1),1);
-Screen('TextSize', window, 40);
+duration = constants.testDur;
 KbQueueStart;
+
 for j = 1:size(data,1)
-    duration = constants.testDur;
     string = '';
     rt = [];
     advance = 0;
-    input = 0; %#ok<NASGU>
+    inputFlag = 0; 
     drawCueTarget(data.cue{j}, '?', window, constants);
     vbl = Screen('Flip', window);
     onset(j) = vbl;
     while GetSecs < onset(j) + duration && advance == 0
-        [string, rt, advance, input] = inputHandler([], string, rt, data.target{j});
-        drawCueTarget(data.cue{j},  string, window, constants);
-        vbl = Screen('Flip', window, vbl + constants.ifi/2);
-        if input
+        [string, rt, advance] = inputHandler([], string, rt, data.target{j});
+        if ~isempty(string) || inputFlag
+            drawCueTarget(data.cue{j},  string, window, constants);
+            vbl = Screen('Flip', window, vbl + constants.ifi/2);
             duration = duration + 3;
-        end
+            inputFlag = 1;
+        else
+            drawCueTarget(data.cue{j}, '?', window, constants);
+            vbl = Screen('Flip', window, vbl + constants.ifi/2);                
+        end        
     end
     [response{j}, firstPress(j), lastPress(j)] = cleanResponses(string, rt);
-    Screen('Flip', window);
 end
+Screen('TextSize', window, oldsize); % reset text size
 end
 
 function [response, firstPress, lastPress] = cleanResponses(string, RT)
