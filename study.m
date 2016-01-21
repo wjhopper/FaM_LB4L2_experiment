2a8a94a31094a8238553b7e46c1759f30e868e06
whopper-cemnl <wjhopper510@gmail.com>
2016-01-21 12:44:20 -0500

    reset the old font size before returning

--- study.m
+++ study.m
@@ -1,7 +1,7 @@
 function onsets = study(data, window, constants)
 
 onsets = nan(size(data,1),1);
-Screen('TextSize', window, 40);
+oldsize = Screen('TextSize', window, 40);
 wakeup = Screen('Flip', window);
 for j = 1:size(data,1);
     drawCueTarget(data.cue{j}, data.target{j}, window, constants)    
@@ -9,6 +9,6 @@ for j = 1:size(data,1);
     onsets(j) = vbl;
 	wakeup = WaitSecs('UntilTime', vbl + constants.cueDur - constants.ifi);
 end
-
+Screen('TextSize', window, oldsize);
 end
 
