67134970fbe9bd1f916e4c0f667563021493589a
whopper-cemnl <wjhopper510@gmail.com>
2016-01-21 12:50:10 -0500

    added better restudy and pratice test countdown text

--- practice.m
+++ practice.m
@@ -4,14 +4,14 @@ for i = 1:2
     studyRows = strcmp(data.practice,'S') & data.pracRound == i;
     testRows = strcmp(data.practice,'T') & data.pracRound == i;
     if strcmp('S', first)
-        countdown('Restudy', 3, constants.countdownSpeed,  window, constants);
+        countdown('Now its time to restudy some pairs from the last list', 3, constants.countdownSpeed,  window, constants);
         data.onset(studyRows) = study(data(studyRows, {'cue','target'}), window, constants);
-        countdown('Practice Test', 3, constants.countdownSpeed,  window, constants);    
+        countdown('It''s time for a practice test on some pairs from the last list', 3, constants.countdownSpeed,  window, constants);    
         [onset, response, firstPress, lastPress] = testing(data(testRows,:), inputHandler, window, constants);
     else
-        countdown('Practice Test', 3, constants.countdownSpeed,  window, constants);
+        countdown('It''s time for a practice test on some pairs from the last list', 3, constants.countdownSpeed,  window, constants);
         [onset, response, firstPress, lastPress] = testing(data(testRows,:), inputHandler, window, constants);
-        countdown('Restudy', 3, constants.countdownSpeed,  window, constants);
+        countdown('Now its time to restudy some pairs from the last list', 3, constants.countdownSpeed,  window, constants);
         data.onset(studyRows) = study(data(studyRows, {'cue','target'}), window, constants);
     end
     data.onset(testRows) = onset;
