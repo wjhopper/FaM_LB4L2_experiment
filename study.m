c75e488e60d21fac501496fa789639a7b09cbd77
whopper <wjhopper510@gmail.com>
2016-01-25 14:53:43 -0500

    added ISI of 500 ms to study functions

--- study.m
+++ study.m
@@ -2,13 +2,14 @@ function onsets = study(data, window, constants)
 
 onsets = nan(size(data,1),1);
 oldsize = Screen('TextSize', window, 40);
-wakeup = Screen('Flip', window);
+wakeup = Screen('Flip', window); % get starting point to base subsequent flips on
 for j = 1:size(data,1);
-    drawCueTarget(data.cue{j}, data.target{j}, window, constants)    
-    vbl = Screen('Flip', window, wakeup + (constants.ifi/2));
-    onsets(j) = vbl;
-	wakeup = WaitSecs('UntilTime', vbl + constants.cueDur - constants.ifi);
+    drawCueTarget(data.cue{j}, data.target{j}, window, constants) % Prepare stimuli
+    onsets(j) = Screen('Flip', window, wakeup + (constants.ifi/2)); % Show stimulus onscreen
+	wakeup = WaitSecs('UntilTime', onsets(j) + constants.cueDur - constants.ifi); % Wait stimulus duration
+    vbl = Screen('Flip', window, wakeup + (constants.ifi/2)); % Clear stimulus
+	wakeup = WaitSecs('UntilTime', vbl + .5 - constants.ifi); % Wait ISI
 end
-Screen('TextSize', window, oldsize);
+Screen('TextSize', window, oldsize); % restore original font size
 end
 
