f7e0e2ec0963cc4a0ce113e4dd67403e132ea8a5
whopper <wjhopper510@gmail.com>
2016-02-03 08:08:52 -0500

    patched LB4L2.bat to work with the local installs of the the experiement and matlab 2015

--- LB4L2.bat
+++ LB4L2.bat
@@ -1,2 +1,2 @@
-cd "C:\Users\lab\My Documents\MATLAB\LB4L2"
-matlab -nodesktop -minimize -r "main; exit;"
+cd "C:\Users\lab\My Documents\MATLAB\whopper\FaM_LB4L2"
+"C:\Program Files\MATLAB\R2015a\bin\matlab.exe" -nodesktop -minimize -r "main; exit;"
